bce43a1dd6956c7547ef60d816fbe13b278b4954
pusok <pusok-11@mail.ru>
2016-12-05 11:18:43 +0300

    nomer2

--- Unit14stroki.cpp
+++ Unit14stroki.cpp
@@ -16,4 +16,22 @@ int main()
     char str[256] = "anapa pos africa";
     char *token;
 
+  
+     token = strtok(str," ");
 
+    if(token[0] == token[strlen(token) - 1]) {
+        count++;
+    }
+
+    while (token != NULL) {
+
+        token = strtok(NULL, " ");
+        if(token != NULL && token[0] == token[strlen(token) - 1]) {
+            count++;
+        }
+    }
+
+    printf("Kolvo slov: %d", count);
+     system("pause");
+    return 0;
+}
